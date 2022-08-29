ef73a33da87c2f3c0eaada0609dd4e223e68bbbd
Guido Cardarelli <guidocarda@hotmail.com>
2022-08-28 21:49:20 -0300

    Migrated from Formik to React Hook Forms

--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -1,6 +1,6 @@
 import { Listbox } from "@headlessui/react";
-import { useField } from "formik";
 import React, { useState } from "react";
+import { useController } from "react-hook-form";
 
 const carreras = [
   { id: 1, name: "Desarrollo de software" },
@@ -10,27 +10,22 @@ const carreras = [
   { id: 5, name: "Traductorado" },
   { id: 6, name: "Recursos Humanos" },
 ];
-const CustomSelect = ({ name, value, onChange }) => {
-  const [selectedOption, setSelectedOption] = useState(carreras[0]);
+const CustomSelect = (props) => {
+  const {
+    field: { value, onChange },
+  } = useController(props);
 
   console.log(value);
   return (
-    <Listbox
-      className="relative "
-      value={selectedOption.name}
-      onChange={setSelectedOption}
-      as="div"
-      type="text"
-      name={name}
-    >
+    <Listbox className="relative" value={value} onChange={onChange} as="div">
       <Listbox.Button className="focus:outline-none mb-4 focus:border-indigo-900 w-full h-10 bg-neutral-800 rounded-md border-neutral-800 border-2 flex items-center justify-between px-2 hover:cursor-pointer">
-        {selectedOption.name}
+        {value?.name ? value?.name : "Selecciona tu carrera!"}
       </Listbox.Button>
       <Listbox.Options className="absolute z-10 top-12 w-full bg-neutral-800 rounded-md border-neutral-800 border-2 flex flex-col gap-2 justify-center p-2 hover:cursor-pointer">
         {carreras.map((carrera) => (
           <Listbox.Option
             className={`focus:outline-none focus:bg-neutral-700 text-left p-2 hover:bg-neutral-700 rounded-sm ${
-              selectedOption.name === carrera.name
+              value?.name === carrera.name
                 ? "bg-indigo-700 hover:bg-indigo-700"
                 : ""
             }`}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useFormik } from "formik";
 import CustomSelect from "../components/CustomSelect";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    control,
+  } = useForm();
+
+  const onSubmit = (data) => alert(JSON.stringify(data));
+
+  console.log(watch("fullName"));
+
   const postFormData = async (formData) => {
     try {
       const docRef = await addDoc(collection(db, "survey"), formData);
@@ -14,68 +26,13 @@ const Home = () => {
     }
   };
 
-  const handleSelectToggle = () => {
-    setIsOpen((isOpen) => !isOpen);
-    // formik.setTouched("degree");
-  };
-
-  const handleSelectChange = (value) => {
-    formik.setFieldValue("degree", value);
-  };
-
-  const validate = (values) => {
-    const errors = {};
-    if (!values.fullName) {
-      errors.fullName = "Required";
-    } else if (values.fullName.length > 15) {
-      errors.fullName = "Must be 15 characters or less";
-    }
-    if (!values.age) {
-      errors.age = "Required";
-    } else if (values.age < 0) {
-      errors.age = "Valor invalido";
-    } else if (values.age > 100) {
-      errors.age = "Ah sos re troll";
-    }
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
-
-    // if (!values.degree) {
-    //   errors.degree = "Required";
-    // }
-    return errors;
-  };
-
-  const formik = useFormik({
-    initialValues: {
-      fullName: "",
-      age: "",
-      email: "",
-      degree: "Select",
-      gameIdea: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      console.log(values);
-      postFormData(values);
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
-  // console.log(formik.values);
-
   return (
     <div className="relative overflow-hidden min-h-screen h-full flex items-center justify-center bg-neutral-900 text-slate-200">
       <div className="absolute w-96 h-96 bg-purple-800 top-5 -right-52 rounded-full filter blur-3xl opacity-20   "></div>
       <div className="absolute w-96 h-96 bg-teal-800 top-29 -left-40 rounded-full filter blur-3xl opacity-10  "></div>
       <div className="absolute w-96 h-96 bg-sky-800 -bottom-2 -right-20 rounded-full filter  blur-3xl opacity-10 "></div>
       <form
-        onSubmit={formik.handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="h-full md:max-w-xl px-4 py-12 flex flex-col justify-center gap-2 relative z-10"
       >
         <p>
@@ -84,36 +41,49 @@ const Home = () => {
         </p>
         <label htmlFor="fullName">Nombre</label>
         <input
-          id="fullName"
-          name="fullName"
-          type="text"
-          {...formik.getFieldProps("fullName")}
+          defaultValue="test"
+          {...register("fullName", { required: true })}
           className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
         />
 
+        {errors.fullName && <span>This field is required</span>}
+
+        <input
+          {...register("age", {
+            required: true,
+            min: { value: 0, message: "the number must be higher than zero" },
+            max: { value: 100, message: "Ah you are so troll" },
+          })}
+          className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
+        />
+
+        {errors.age && <span>{errors.age.message}</span>}
+
+        <CustomSelect
+          name={"carreras"}
+          control={control}
+          rules={{ required: true }}
+        ></CustomSelect>
+        {/*         
         {formik.touched.fullName && formik.errors.fullName ? (
           <div> {formik.errors.fullName}</div>
-        ) : null}
+        ) : null} 
 
         <label htmlFor="age">Edad</label>
         <input
           id="age"
           name="age"
           type="text"
-          pattern="[0-9]*"
-          min={0}
-          max={100}
-          {...formik.getFieldProps("age")}
           className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
         />
 
-        {formik.touched.age && formik.errors.age ? (
+   {formik.touched.age && formik.errors.age ? (
           <div>{formik.errors.age}</div>
         ) : null}
 
-        <CustomSelect {...formik.getFieldProps("degree")}></CustomSelect>
+        
 
-        {formik.errors.degree ? (
+         {formik.errors.degree ? (
           <div className=" text-red-600 text-sm">{formik.errors.degree}</div>
         ) : null}
 
@@ -125,14 +95,13 @@ const Home = () => {
             id="email"
             name="email"
             type="text"
-            {...formik.getFieldProps("email")}
             className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
           />
-          {formik.touched.email && formik.errors.email ? (
+       {formik.touched.email && formik.errors.email ? (
             <div className="absolute bottom-0 text-red-600 text-sm">
               {formik.errors.email}
             </div>
-          ) : null}
+          ) : null} 
         </div>
 
         <div className="text-left flex flex-col gap-2 my-4">
@@ -157,12 +126,11 @@ const Home = () => {
           id="gameIdea"
           name="gameIdea"
           type="text"
-          {...formik.getFieldProps("gameIdea")}
           className="focus:outline-none focus:border-indigo-900  bg-white/5 border-2 border-neutral-800 rounded-md h-28 w-full p-2 resize-none"
         />
-        {formik.touched.gameIdea && formik.errors.gameIdea ? (
+      {formik.touched.gameIdea && formik.errors.gameIdea ? (
           <div>{formik.errors.gameIdea}</div>
-        ) : null}
+        ) : null}  */}
 
         <button
           type="submit"
