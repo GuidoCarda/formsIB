ac15ff97b19191ae518ee54947d9f78ff1722280
Guido Cardarelli <guidocarda@hotmail.com>
2022-08-30 21:17:59 -0300

    boilerplate code deleted, dropdown styles and logic improved

--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -1,4 +1,4 @@
-import { Listbox } from "@headlessui/react";
+import { Listbox, Transition } from "@headlessui/react";
 import React from "react";
 import { useController } from "react-hook-form";
 
@@ -18,26 +18,49 @@ const CustomSelect = (props) => {
   console.log(value);
   return (
     <Listbox className="relative" value={value} onChange={onChange} as="div">
-      <Listbox.Button className="focus:outline-none mb-4 focus:border-indigo-900 w-full h-10 bg-neutral-800 rounded-md border-neutral-800 border-2 flex items-center justify-between px-2 hover:cursor-pointer">
-        {value?.name ? value?.name : "Selecciona tu carrera!"}
+      <Listbox.Button className="focus:outline-none mb-4 focus:border-indigo-900 w-full h-10 bg-neutral-900  rounded-md border-neutral-800 border-2 flex items-center justify-between px-2 hover:cursor-pointer">
+        {value ? value : "Selecciona tu carrera!"}
       </Listbox.Button>
-      <Listbox.Options className="absolute z-10 top-12 w-full bg-neutral-800 rounded-md border-neutral-800 border-2 flex flex-col gap-2 justify-center p-2 hover:cursor-pointer">
-        {carreras.map((carrera) => (
-          <Listbox.Option
-            className={`focus:outline-none focus:bg-neutral-700 text-left p-2 hover:bg-neutral-700 rounded-sm ${
-              value?.name === carrera.name
-                ? "bg-indigo-700 hover:bg-indigo-700"
-                : ""
-            }`}
-            key={carrera.id}
-            value={carrera}
-          >
-            {carrera.name}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+      <Transition
+        enter="transition duration-300 ease-out"
+        enterFrom="transform scale-90 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-300 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Listbox.Options className="outline-indigo-600 absolute z-10  w-full bg-neutral-800 rounded-md border-neutral-800 border-2 flex flex-col gap-2 justify-center p-2 hover:cursor-pointer">
+          {carreras.map((carrera) => (
+            <Listbox.Option
+              className={({ active }) =>
+                `relative cursor-default select-none text-left p-2 rounded-sm ${
+                  active ? "bg-neutral-700 text-gray-300" : "text-gray-300"
+                } ${
+                  value === carrera.name ? "bg-indigo-500 text-indigo-300" : ""
+                }`
+              }
+              key={carrera.id}
+              value={carrera.name}
+            >
+              {carrera.name}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Transition>
     </Listbox>
   );
 };
 
 export default CustomSelect;
+
+{
+  /* <Listbox.Option
+className={`focus:outline-none focus:bg-neutral-500 text-left p-2 hover:bg-neutral-700 rounded-sm ${
+  value?.name === carrera.name
+    ? "bg-indigo-700 hover:bg-indigo-700"
+    : ""
+}`}
+key={carrera.id}
+value={carrera}
+> */
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,10 +37,10 @@ const Home = () => {
   };
 
   return (
-    <div className="relative overflow-hidden min-h-screen h-full flex items-center justify-center bg-neutral-900 text-slate-200">
-      <div className="absolute w-52 h-52 bg-purple-500/50 top-5 -right-20 rounded-full filter blur-3xl  "></div>
+    <div className="relative overflow-hidden min-h-screen h-full flex items-center justify-center bg-neutral-900 text-slate-200 py-2">
+      <div className="absolute w-52 h-52 bg-purple-500 top-5 -right-20 rounded-full filter blur-2xl  opacity-10 "></div>
       <div className="absolute w-96 h-96 bg-teal-500 top-29 -left-40 rounded-full filter blur-2xl opacity-10  "></div>
-      <div className="absolute w-96 h-96 bg-sky-500 -bottom-2 -right-20 rounded-full filter  blur-2xl opacity-10 "></div>
+      <div className="absolute w-96 h-96 bg-violet-500 -bottom-2 -right-20 rounded-full filter  blur-2xl opacity-10 "></div>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="h-full md:max-w-xl px-4 py-12 flex flex-col justify-center gap-2 relative z-10"
@@ -53,20 +53,28 @@ const Home = () => {
         <input
           {...register("fullName", { required: true })}
           placeholder="Rosa melano"
-          className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
+          className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
         />
 
         {errors.fullName && <span>Este campo es requerido</span>}
 
         <label htmlFor="email">Email ( Contacto ) </label>
         <input
-          {...register("email", { required: true })}
+          {...register("email", {
+            required: true,
+            pattern: { value: /^\S+@\S+$/i, message: "Mail invalido" },
+          })}
           placeholder="rosamelano@gmail.com"
-          className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
+          className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
         />
 
-        {errors.email && <span>Este campo es requerido</span>}
-
+        {errors.email && (
+          <span>
+            {errors?.email?.message
+              ? errors?.email?.message
+              : "Campo requerido"}
+          </span>
+        )}
         <div className="relative">
           <label htmlFor="age">Edad</label>
           <input
@@ -76,7 +84,7 @@ const Home = () => {
               max: { value: 100, message: "Ah sos re troll" },
             })}
             placeholder="20"
-            className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-10"
+            className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-10"
           />
 
           {errors.age && (
@@ -106,7 +114,7 @@ const Home = () => {
             <label htmlFor="gamesPlayed">Que juegos soles jugar?</label>
             <input
               {...register("gamesPlayed", { required: plays ? true : false })}
-              className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
+              className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
             />
             {errors.gamesPlayed && <span>Este campo es requerido</span>}
           </>
@@ -131,75 +139,8 @@ const Home = () => {
         <textarea
           placeholder="Un videojuego ambientado en xxx que se trate de xxx y que el objetivo sea xxx ... Vo me entende, algo asi. Iluminanos"
           {...register("gameIdea", { required: true })}
-          className="focus:outline-none focus:border-indigo-900  bg-white/5 border-2 border-neutral-800 rounded-md h-28 w-full p-2 resize-none"
+          className="focus:outline-none focus:border-indigo-900  bg-neutral-900 border-2 border-neutral-800 rounded-md h-28 w-full p-2 resize-none"
         />
-        {/*         
-        {formik.touched.fullName && formik.errors.fullName ? (
-          <div> {formik.errors.fullName}</div>
-        ) : null} 
-
-        <label htmlFor="age">Edad</label>
-        <input
-          id="age"
-          name="age"
-          type="text"
-          className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
-        />
-
-   {formik.touched.age && formik.errors.age ? (
-          <div>{formik.errors.age}</div>
-        ) : null}
-
-        
-
-         {formik.errors.degree ? (
-          <div className=" text-red-600 text-sm">{formik.errors.degree}</div>
-        ) : null}
-
-        <div className="relative">
-          <label htmlFor="email" className="">
-            Email
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            className="focus:outline-none focus:border-indigo-900 bg-white/5 border-2 border-neutral-800 h-10 rounded-md w-full px-2 mb-6"
-          />
-       {formik.touched.email && formik.errors.email ? (
-            <div className="absolute bottom-0 text-red-600 text-sm">
-              {formik.errors.email}
-            </div>
-          ) : null} 
-        </div>
-
-        <div className="text-left flex flex-col gap-2 my-4">
-          <p>
-            Como proyecto final de programacion debemos llevar a cabo el
-            desarrollo de un videojuego y nos encontramos con que no tenemos ni
-            idea de cual podria ser la tematica del mismo.
-          </p>
-
-          <p>
-            Por eso necesitamos tu ayuda y desarrollamos este cuestionario para
-            que nos puedas dejarnos tus ideas.
-          </p>
-
-          <p>Tene en cuenta que debe ser un videojuego en 3D.</p>
-        </div>
-
-        <label htmlFor="gameIdea" className="inline-block ">
-          Game idea
-        </label>
-        <textarea
-          id="gameIdea"
-          name="gameIdea"
-          type="text"
-          className="focus:outline-none focus:border-indigo-900  bg-white/5 border-2 border-neutral-800 rounded-md h-28 w-full p-2 resize-none"
-        />
-      {formik.touched.gameIdea && formik.errors.gameIdea ? (
-          <div>{formik.errors.gameIdea}</div>
-        ) : null}  */}
 
         <button
           type="submit"
