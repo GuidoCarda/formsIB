ffcd696c9d8c9744dc2ba0b1b199fba200d491fb
Guido Cardarelli <guidocarda@hotmail.com>
2022-09-01 16:01:51 -0300

    Game Selection styles improved

--- src/components/GameSelect.jsx
+++ src/components/GameSelect.jsx
@@ -1,43 +1,88 @@
-import { useState } from 'react'
-import { RadioGroup } from '@headlessui/react'
+import { useState } from "react";
+import { RadioGroup } from "@headlessui/react";
 
 function GameSelect() {
-  let [plan, setPlan] = useState('startup')
+  const [plan, setPlan] = useState("startup");
 
   return (
-    <RadioGroup className="content-center flex justify-around justify-items-center gap-4 accent-indigo-500" value={plan} onChange={setPlan}>
-      <RadioGroup.Option value="valorant" className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-auto " >
-          {({ checked }) => (
-          <span className={`flex items-center h-full px-4 ${checked ? 'bg-blue-200' : ''}`}>Valorant</span>
-          )}
-      </RadioGroup.Option>
-      <RadioGroup.Option  className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-auto " value="lol">
+    <RadioGroup
+      className=" flex gap-4 accent-indigo-500"
+      value={plan}
+      onChange={setPlan}
+    >
+      <RadioGroup.Option
+        value="valorant"
+        className=" bg-neutral-800 border-2 border-neutral-900 rounded-md  overflow-hidden "
+      >
         {({ checked }) => (
-          <span className={`flex items-center h-full px-4 ${checked ? 'bg-blue-200' : ''}`}>Lol</span>
+          <span
+            className={`flex items-center h-full px-4 ${
+              checked ? "bg-indigo-800 text-white" : ""
+            }`}
+          >
+            Valorant
+          </span>
         )}
       </RadioGroup.Option>
-      
-      <RadioGroup.Option  className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-auto " value="dota">
+      <RadioGroup.Option
+        className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 rounded-md  "
+        value="lol"
+      >
         {({ checked }) => (
-            <span className={`flex items-center h-full px-4 ${checked ? 'bg-blue-200' : ''}`}>Dota</span>
-
+          <span
+            className={`flex items-center h-full px-4 ${
+              checked ? "bg-indigo-800 text-white" : ""
+            }`}
+          >
+            Lol
+          </span>
         )}
       </RadioGroup.Option>
-      <RadioGroup.Option  className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-auto "value="nfs">
-        {({ checked }) => (
-          <span className={`flex items-center h-full px-4 ${checked ? 'bg-blue-200' : ''}`}>NFS</span>
 
+      <RadioGroup.Option
+        className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800  rounded-md  "
+        value="dota"
+      >
+        {({ checked }) => (
+          <span
+            className={`flex items-center h-full px-4 ${
+              checked ? "bg-indigo-800 text-white" : ""
+            }`}
+          >
+            Dota
+          </span>
         )}
       </RadioGroup.Option>
-      <RadioGroup.Option className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-auto " value="ashe">
+      <RadioGroup.Option
+        className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 rounded-md  "
+        value="nfs"
+      >
         {({ checked }) => (
-          <span className={`flex items-center h-full px-4 ${checked ? 'bg-blue-200' : ''}`}>Ashe</span>
-
+          <span
+            className={`flex items-center h-full px-4 ${
+              checked ? "bg-indigo-800 text-white" : ""
+            }`}
+          >
+            NFS
+          </span>
+        )}
+      </RadioGroup.Option>
+      <RadioGroup.Option
+        className="focus:outline-none focus:border-indigo-900 bg-neutral-900 border-2 border-neutral-800 h-10 rounded-md w-auto "
+        value="ashe"
+      >
+        {({ checked }) => (
+          <span
+            className={`flex items-center h-full px-4 ${
+              checked ? "bg-indigo-800 text-white" : ""
+            }`}
+          >
+            Ashe
+          </span>
         )}
       </RadioGroup.Option>
     </RadioGroup>
-  )
+  );
 }
 
-  
 export default GameSelect;
