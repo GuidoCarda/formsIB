2ce6348ef55654a2328deb554473ed48667f04b3
Guido Cardarelli <guidocarda@hotmail.com>
2022-09-02 17:02:08 -0300

    ReactRouter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
